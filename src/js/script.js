924d466039b70ff2f54a3146acc07b5721df3fd0
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-02-01 01:00:02 +0100

    add burger menu mobile —editing part 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-console.log('prout')
+
+/************ BURGER MENU ************/
+
+var burger = document.querySelector('.container');
+var menu = document.querySelector('.menu');
+var header = document.querySelector('.header')
+
+burger.addEventListener('click', function () {
+    burger.classList.toggle('is-click')
+    menu.classList.toggle('is-open-menu')
+    header.classList.toggle('header-is-open')
+});
